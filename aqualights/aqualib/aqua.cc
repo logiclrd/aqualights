35fdc1eb074e32ee617b32372e2be4d2147b1172
Jonathan Gilbert <JonathanG@iQmetrix.com>
2018-07-29 11:09:41 -0500

    Began implementation of aqualights projects. - Added project aqualib with a port of Falstad's ripple simulator code from Jabbascript. - Added project aquadll to expose aqualib as a shared library to be called from the test driver. - Added project debugui to host a WPF test driver for development work. - Added .editorconfig and .gitignore.

---
+++ aqualights/aqualib/aqua.cc
@@ -0,0 +1,211 @@
+#include "aqua.h"
+#include "math.h"
+
+void aqua_initialize(AquaContext *context, int width, int height, float frames_per_cycle)
+{
+	context->window_width = width;
+	context->window_height = height;
+
+	int border = width / 9;
+
+	if (border < 20)
+		border = 20;
+
+	context->window_offset_x = border;
+	context->window_offset_y = border;
+
+	context->grid_size_x = context->window_width + context->window_offset_x * 2;
+	context->grid_size_y = context->window_height + context->window_offset_y * 2;
+
+	context->gw = context->grid_size_y;
+
+	context->window_right = context->window_offset_x + context->window_width - 1;
+	context->window_bottom = context->window_offset_y + context->window_height - 1;
+
+	context->grid_size_xy = context->grid_size_x * context->grid_size_y;
+
+	context->func = new float[context->grid_size_xy];
+	context->funci = new float[context->grid_size_xy];
+	context->damp = new float[context->grid_size_xy];
+
+	for (int i = 0; i < context->grid_size_xy; i++)
+		context->damp[i] = 1.0f;
+
+	for (int i = 0; i != context->window_offset_x; i++)
+		for (int j = 0; j != context->grid_size_x; j++)
+			context->damp[i + j * context->gw] = context->damp[context->grid_size_x - 1 - i + context->gw * j] = context->damp[j + context->gw * i] = context->damp[j + (context->grid_size_y - 1 - i) * context->gw] = (.999 - (context->window_offset_x - i) * .002);
+
+	context->time_factor = 6.283185f / frames_per_cycle;
+}
+
+void aqua_update_ripple(AquaContext *context)
+{
+	int mxx = context->grid_size_x - 1;
+	int mxy = context->grid_size_y - 1;
+
+	int jstart;
+	int jend;
+	int jinc;
+
+	if (context->move_down)
+	{
+		jstart = 1;
+		jend = mxy;
+		jinc = 1;
+		context->move_down = false;
+	}
+	else
+	{
+		jstart = mxy - 1;
+		jend = 0;
+		jinc = -1;
+		context->move_down = true;
+	}
+
+	bool move_right = context->move_down;
+
+	float sinhalfth = sinf(0.125f);
+	float sinth = sinf(0.25f);
+	float scaleo = 1 - sqrtf(4 * sinhalfth * sinhalfth - sinth * sinth);
+
+	for (int j = jstart; j != jend; j += jinc)
+	{
+		int istart;
+		int iend;
+		int iinc;
+
+		if (move_right)
+		{
+			iinc = 1;
+			istart = 1;
+			iend = mxx;
+
+			move_right = false;
+		}
+		else
+		{
+			iinc = -1;
+			istart = mxx - 1;
+			iend = 0;
+
+			move_right = true;
+		}
+
+		int gi = j * context->gw + istart;
+		int giEnd = j * context->gw + iend;
+
+		for (; gi != giEnd; gi += iinc) {
+			float previ = context->func[gi - 1];
+			float nexti = context->func[gi + 1];
+			float prevj = context->func[gi - context->gw];
+			float nextj = context->func[gi + context->gw];
+
+			float basis = (nexti + previ + nextj + prevj) * .25;
+			
+			float a = context->func[gi] - basis;
+			float b = context->funci[gi];
+
+			float damp = context->damp[gi];
+
+			if (damp != 1)
+			{
+				a *= damp;
+				b *= damp;
+			}
+
+			context->func[gi] = basis + a * scaleo - b * sinth;
+			context->funci[gi] = b * scaleo + a * sinth;
+		}
+	}
+}
+
+void aqua_add_source(AquaContext *context, AquaSource source)
+{
+	source.fade_ratio = 1.0f / (source.duration * 0.25f);
+
+	AquaSource *link = new AquaSource(source);
+
+	link->next = context->first_source;
+	context->first_source = link;
+}
+
+void aqua_update_sources(AquaContext *context)
+{
+	AquaSource **ref = &context->first_source;
+
+	for (AquaSource *source = context->first_source; source != NULL; ref = &source->next, source = source->next)
+	{
+top_of_loop:
+		source->local_time++;
+
+		if (source->local_time > source->duration)
+		{
+			*ref = source->next;
+			delete source;
+			source = *ref;
+
+			if (source == NULL)
+				break;
+
+			goto top_of_loop;
+		}
+
+		float lifetime_intensity = 1.0;
+
+		if (source->local_time < source->duration / 4)
+			lifetime_intensity = source->local_time * source->fade_ratio;
+		if (source->local_time >= source->duration * 3 / 4)
+			lifetime_intensity = (source->duration - source->local_time) * source->fade_ratio;
+
+		float intensity = source->intensity * lifetime_intensity * sinf(source->local_time * context->time_factor);
+
+		int start_y = (int)floorf(source->y - source->radius);
+		int end_y = (int)floorf(source->y + source->radius);
+
+		if ((start_y < context->grid_size_y) && (end_y >= 0))
+		{
+			if (start_y < 0)
+				start_y = 0;
+			if (end_y >= context->grid_size_y)
+				end_y = context->grid_size_y - 1;
+
+			for (int y = start_y; y <= end_y; y++)
+			{
+				float sector_radius = fabsf(y - source->y);
+
+				if (sector_radius >= source->radius)
+					continue;
+
+				float sector_width = sqrtf(source->radius * source->radius - sector_radius * sector_radius);
+
+				int start_x = source->x - sector_width;
+				int end_x = source->x + sector_width;
+
+				if ((start_x < context->grid_size_x) && (end_x >= 0))
+				{
+					if (start_x < 0)
+						start_x = 0;
+					if (end_x >= context->grid_size_x)
+						end_x = context->grid_size_x - 1;
+
+					for (int x = start_x, d = y * context->gw + x; x <= end_x; x++, d++)
+					{
+						context->func[d] = intensity;
+						context->funci[d] = 0;
+					}
+				}
+			}
+		}
+
+		source->x += source->dx;
+		source->y += source->dy;
+	}
+}
+
+// TODO: voronoi mapping of light positions
+// TODO: render to lights as brightness
+// TODO: convert brightness to palette
+// TODO: palette rotation functions
+//       => long day/night cycle
+//       => short, stochastic sun/shade cycle during day
+// TODO: create sources randomly
---
+++ aqualights/aqualib/aqua.h
@@ -0,0 +1,36 @@
+struct AquaContext
+{
+	int window_width, window_height;
+	int window_offset_x, window_offset_y;
+	int window_right, window_bottom;
+	int grid_size_x, grid_size_y;
+	int grid_size_xy;
+	int gw;
+
+	bool move_down;
+
+	float *func;
+	float *funci;
+	float *damp;
+
+	float time_factor;
+
+	AquaSource *first_source;
+};
+
+struct AquaSource
+{
+	float x, y;
+	float dx, dy;
+
+	float radius;
+	float intensity;
+
+	int local_time, duration;
+
+	float fade_ratio;
+
+	struct AquaSource *next;
+};
+
+typedef struct AquaContext AquaContext;
